be3e1f9bde98989faf9bdc2745c40181187f9fbd
HaidraSuleiman <h.souleman@icloud.com>
2026-05-01 14:56:26 +0100

    preparing interactive header that reflects users authentication

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,3 +1,5 @@
 "use server";
 
 export async function createComment() {}
+
+//revalidate the post show page
--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,6 @@
 "use server";
 
 export async function createTopic() {}
+//revalidate paths : HOME PAGE
+
+// Timer for revalidating
